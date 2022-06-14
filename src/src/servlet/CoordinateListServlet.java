b7d3fd03836a6dc2a6c55b7cf754e2b4668c9dc2
dojo2022watanabeayae <103238534+dojo2022watanabeayae@users.noreply.github.com>
2022-06-14 14:13:21 +0900

    errorServlet

--- src/src/servlet/CoordinateListServlet.java
+++ src/src/servlet/CoordinateListServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ItemDetailServlet
  */
-@WebServlet("CoordinateListServlet")
+@WebServlet("/CoordinateListServlet")
 public class CoordinateListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
