e231e4ce3caa1b669f453b55e22b5cdaf00f50ad
dojo2022-inoue <103237344+dojo2022-inoue@users.noreply.github.com>
2022-06-14 15:12:25 +0900

    Update NewItemServlet.java

--- src/src/servlet/NewItemServlet.java
+++ src/src/servlet/NewItemServlet.java
@@ -1,5 +1,15 @@
 package servlet;
 
-public class NewItemServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/NewItemServlet")
+public class NewItemServlet {
+protected void doGet(HttpServletRequest request,HttpServletResponse response)
+throws ServletException, IOException {
+
+}
 }
