253e8c080747c5b1f2d4b68b483497c678ab3b0f
dojo2022-kakuichi <103239376+dojo2022-kakuichi@users.noreply.github.com>
2022-06-15 17:15:58 +0900

    CoordinateDAO　編集01

--- src/src/dao/CoordinateDAO.java
+++ src/src/dao/CoordinateDAO.java
@@ -24,8 +24,8 @@ public class CoordinateDAO{
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/CCC", "sa", "ccc");
 
 			// SELECT文を準備する
-			String sql ="select top(3) COORDINATE_ID, COORDINATE from COORDINATE where SEASON = ? and PURPOSE = bussines";
-			String sql2 ="select top(3) COORDINATE_ID, COORDINATE from COORDINATE where SEASON = ? and PURPOSE = private";
+			String sql ="select top(3) USER_ID, COORDINATE_ID, SEAAON, PURPOSE, COORDINATE_IMAGE from COORDINATE where SEASON = ? and PURPOSE = bussines";
+			String sql2 ="select top(3) USER_ID, COORDINATE_ID, SEAAON, PURPOSE, COORDINATE_IMAGE from COORDINATE where SEASON = ? and PURPOSE = private";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 			PreparedStatement pStmt2 = conn.prepareStatement(sql2);
 
@@ -48,13 +48,19 @@ public class CoordinateDAO{
 			// 結果表をコレクションにコピーする
 			while (rs.next()) {
 				CoordinateModel card = new CoordinateModel(
-				rs.getString("COORDINATE_ID"),
+				rs.getString("user_id"),
+				rs.getString("coordinate_id"),
+				rs.getString("SEASON"),
+				rs.getString("purpose"),
 				rs.getString("COORDINATE_IMAGE")
 				);
 				coordinateList.add(card);
 
 				CoordinateModel card2 = new CoordinateModel(
-				rs2.getString("COORDINATE_ID"),
+				rs2.getString("user_id"),
+				rs2.getString("coordinate_id"),
+				rs2.getString("SEASON"),
+				rs2.getString("purpose"),
 				rs2.getString("COORDINATE_IMAGE")
 				);
 				coordinateList.add(card2);
@@ -91,13 +97,13 @@ public class CoordinateDAO{
 
 
 	}
-}
 
 
 
 
 
-//引数cardで指定されたレコードを登録し、成功したらtrueを返す
+
+	//コーディネートの登録
 	public boolean insert(CoordinateModel card) {
 		Connection conn = null;
 		boolean result = false;
@@ -110,51 +116,43 @@ public class CoordinateDAO{
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/CCC", "sa", "ccc");
 
 			// SQL文を準備する
-			String sql = "insert into COORDINATE (USER_ID, COORDINATE_ID, SEASON, ) values (?, ?, ?, ?, ?, ?, ?)";
+			String sql = "insert into COORDINATE (USER_ID, COORDINATE_ID, SEASON, PURPOSE, COORDINATE_IMAGE) values (?, ?, ?, ?, ?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
-			if (card.getNumber() != null && !card.getNumber().equals("")) {
-				pStmt.setString(1, card.getNumber());
+			if (card.getUser_id() != null && !card.getUser_id().equals("")) {
+				pStmt.setString(1, card.getUser_id());
 			}
 			else {
 				pStmt.setString(1, null);
 			}
-			if (card.getName() != null && !card.getName().equals("")) {
-				pStmt.setString(2, card.getName());
+
+			if (card.getCoordinate_id() != null && !card.getCoordinate_id().equals("")) {
+				pStmt.setString(2, card.getCoordinate_id());
 			}
 			else {
 				pStmt.setString(2, null);
 			}
-			if (card.getAddress() != null && !card.getAddress().equals("")) {
-				pStmt.setString(3, card.getAddress());
+
+			if (card.getSeason() != null && !card.getSeason().equals("")) {
+				pStmt.setString(3, card.getSeason());
 			}
 			else {
 				pStmt.setString(3, null);
 			}
-			if (card.getTell() != null && !card.getTell().equals("")) {
-				pStmt.setString(4, card.getTell());
+
+			if (card.getPurpose() != null && !card.getPurpose().equals("")) {
+				pStmt.setString(4, card.getPurpose());
 			}
 			else {
 				pStmt.setString(4, null);
 			}
-			if (card.getGender() != null && !card.getGender().equals("")) {
-				pStmt.setString(5, card.getGender());
+
+			if (card.getCoordinate_image() != null && !card.getCoordinate_image().equals("")) {
+				pStmt.setString(5, card.getCoordinate_image());
 			}
 			else {
 				pStmt.setString(5, null);
-			}
-			if (card.getEmail() != null && !card.getEmail().equals("")) {
-				pStmt.setString(6, card.getEmail());
-			}
-			else {
-				pStmt.setString(6, null);
-			}
-			if (card.getCompany() != null && !card.getCompany().equals("")) {
-				pStmt.setString(7, card.getCompany());
-			}
-			else {
-				pStmt.setString(7, null);
 			}
 
 			// SQL文を実行する
@@ -162,12 +160,68 @@ public class CoordinateDAO{
 				result = true;
 			}
 		}
+
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
+
 		catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+			// 結果を返す
+			return result;
+	}
+
+
+
+
+	// コーディネートの削除
+	public boolean delete(String coodinate_id) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/CCC", "sa", "ccc");
+
+			// SQL文を準備する
+			String sql = "delete from COORDINATE where COORDINATE_id=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setString(1, coodinate_id);
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
 		finally {
 			// データベースを切断
 			if (conn != null) {
@@ -183,3 +237,8 @@ public class CoordinateDAO{
 		// 結果を返す
 		return result;
 	}
+
+
+
+}
+
