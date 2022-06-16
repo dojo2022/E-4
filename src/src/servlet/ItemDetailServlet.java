0c2763f58864d3e72007f18211016e83541d0341
dojo2022-takahashi <103239301+dojo2022-takahashi@users.noreply.github.com>
2022-06-16 16:34:15 +0900

    ItemDetail完成（仮）

--- src/src/servlet/ItemDetailServlet.java
+++ src/src/servlet/ItemDetailServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.CoordinateDAO;
+
 /**
  * Servlet implementation class ItemDetailServlet
  */
@@ -20,7 +22,14 @@ public class ItemDetailServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setCharacterEncoding("UTF-8");
+        String item_id = request.getParameter("q");
+        // 検索処理を行う
+        Coordinate_ItemDAO itemDao = new Coordinate_itemDAO();
+        List<Coordinate_Item> itemList = itemDao.select(new Coordinate_Item("", item_id,  "",  "",  "",  "",  "",  "",  ""));
+        // 検索結果をリクエストスコープに格納する
+        request.setAttribute("itemList", itemList);
+        // 結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ItemDetail.jsp");
 		dispatcher.forward(request, response);
 	}
@@ -29,6 +38,37 @@ public class ItemDetailServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // リクエストパラメータを取得する
+        request.setCharacterEncoding("UTF-8");
+        String item_id = request.getParameter("item_id");
+        String brand = request.getParameter("brand");
+        String size = request.getParameter("size");
+        String size = request.getParameter("remarks");
 
-	}
+        // 更新または削除を行う
+        CoordinateDAO itemDao = new CoordinateDAO();
+        if (request.getParameter("submit").equals("更新")) {
+            if (itemDao.update(new Coordinate_Item(item_id, brand, size, remarks))) {  // 更新成功
+                request.setAttribute("result",
+                new Result("レコードを更新しました。", "/CCC/itemDetailServlet"));
+            }
+            else {                                              // 更新失敗
+                request.setAttribute("result",
+                new Result("レコードを更新できませんでした。", "/CCC/itemDetailServlet"));
+            }
+        }
+        else if(request.getParameter("submit").equals("削除")){
+            if (itemDao.delete(item_id)) { // 削除成功
+                request.setAttribute("result",
+                new Result("レコードを削除しました。", "/CCC/itemDetailServlet"));
+            }
+            else {                      // 削除失敗
+                request.setAttribute("result",
+                new Result("レコードを削除できませんでした。", "/CCC/itemDetailServlet"));
+            }
+        }
+        // 結果ページにフォワードする
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/NewItemError.jsp");
+        dispatcher.forward(request, response);
+    }
 }
