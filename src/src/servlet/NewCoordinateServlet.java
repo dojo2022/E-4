24c22f6e6d8cddb3eac4848a5996fa750a43510e
dojo2022-inoue <103237344+dojo2022-inoue@users.noreply.github.com>
2022-06-17 16:53:09 +0900

    Update

--- src/src/servlet/NewCoordinateServlet.java
+++ src/src/servlet/NewCoordinateServlet.java
@@ -9,10 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.CoordinateDAO;
+
 /**
  * Servlet implementation class HomeServlet
  */
 @WebServlet("/NewCoordinateServlet")
+
 public class NewCoordinateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -20,6 +23,14 @@ public class NewCoordinateServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        request.setCharacterEncoding("UTF-8");
+	        String coordinate_id = request.getParameter("q");
+	        // 検索処理を行う
+	        CoordinateDAO itemDao = new CoordinateDAO();
+	        List<CoordinateItem> itemList = itemDao.select(new Coordinate_Item("", "",  "",  "",  "",  "",  "",  "",  ""));
+
+	        request.setAttribute("itemList", itemList);
+	        // 結果ページにフォワードする
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/NewCoordinate.jsp");
 		dispatcher.forward(request, response);
@@ -28,7 +39,13 @@ public class NewCoordinateServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String user_id = request.getParameter("user_id");
+        String Coordinate_id = request.getParameter("Coordinate_id");
+        String Season = request.getParameter("Season");
+        String Purpose = request.getParameter("Purpose");
+        String Coordinate_image = request.getParameter("Coordinate_image");
 	}
 }
