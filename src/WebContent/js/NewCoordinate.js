36d48dcd31aa264eae76af713dfdb78f89fa10dd
dojo2022-kakuichi <103239376+dojo2022-kakuichi@users.noreply.github.com>
2022-06-23 17:47:06 +0900

    JSPファイル、CSSファイル修正

--- src/WebContent/js/NewCoordinate.js
+++ src/WebContent/js/NewCoordinate.js
@@ -1,6 +1,7 @@
 var image = document.getElementById("uploadimage");
 var imageError1 = document.getElementById("imageError");
 var imageplace = document.getElementById("imageplace");
+var div1 = document.getElementById("div1");
 
 function imageError(){
 	var src = uploadimage.getAttribute("src");
@@ -11,6 +12,11 @@ function imageError(){
 	}
 }
 
-
+function buttonClick(){
+	const div1 = document.getElementById("div1");
+	if(!div1.hasChildNobes()){
+		div11.innerHTML += "<div class = imageError><p id = error2>画像を挿入してください</p></div>"
+	}
+}
 
 
